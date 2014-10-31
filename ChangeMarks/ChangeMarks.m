79a45dcd662c5749f9fdc90cd91f556c567f0eba
Christoffer Winterkvist <chris@zenangst.com>
2014-10-31 19:10:14 +0100

    Cleanup change marks

--- ChangeMarks/ChangeMarks.m
+++ ChangeMarks/ChangeMarks.m
@@ -21,11 +21,11 @@ static ChangeMarks *sharedPlugin;
 {
     static dispatch_once_t onceToken;
     NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
-    if ([currentApplicationName isEqual:@"Xcode"]) {
-        dispatch_once(&onceToken, ^{
-            sharedPlugin = [[self alloc] initWithBundle:plugin];
-        });
-    }
+    if (![currentApplicationName isEqual:@"Xcode"]) return;
+    
+    dispatch_once(&onceToken, ^{
+        sharedPlugin = [[self alloc] initWithBundle:plugin];
+    });
 }
 
 + (instancetype)sharedPlugin
@@ -35,29 +35,12 @@ static ChangeMarks *sharedPlugin;
 
 - (id)initWithBundle:(NSBundle *)plugin
 {
-    if (self = [super init]) {
-        // reference to plugin's bundle, for resource access
-        self.bundle = plugin;
-        
-        // Create menu items, initialize UI, etc.
+    self = [super init];
+    if (!self) return nil;
 
-        // Sample Menu Item:
-        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
-        if (menuItem) {
-            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
-            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
-            [actionMenuItem setTarget:self];
-            [[menuItem submenu] addItem:actionMenuItem];
-        }
-    }
+    self.bundle = plugin;
+
     return self;
-}
-
-// Sample Action, for menu item:
-- (void)doMenuAction
-{
-    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
-    [alert runModal];
 }
 
 - (void)dealloc
