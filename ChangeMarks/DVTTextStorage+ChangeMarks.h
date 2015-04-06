7e8e846b7d39338ecb7fcad1853f0d13e8c9bca0
Christoffer Winterkvist <chris@zenangst.com>
2015-04-06 19:49:44 +0200

    Remove Xcode Manager

---
+++ ChangeMarks/DVTTextStorage+ChangeMarks.h
@@ -0,0 +1,13 @@
+//
+//  DVTTextStorage+ChangeMarks.h
+//  ChangeMarks
+//
+//  Created by Christoffer Winterkvist on 06/04/15.
+//  Copyright (c) 2015 zenangst. All rights reserved.
+//
+
+
+
+@interface DVTTextStorage (ChangeMarks)
+
+@end
