a0374b9399616fcbeb76bfa7e8faf3448f17cbd6
Christoffer Winterkvist <chris@zenangst.com>
2015-04-06 19:51:43 +0200

    Organize project

--- ChangeMarks/DVTTextStorage+ChangeMarks.m
+++ ChangeMarks/DVTTextStorage+ChangeMarks.m
@@ -15,7 +15,7 @@
 - (void)zen_replaceCharactersInRange:(NSRange)range withString:(NSString *)string withUndoManager:(id)undoManager
 {
     [self zen_replaceCharactersInRange:range withString:string withUndoManager:undoManager];
-    
+
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [[NSNotificationCenter defaultCenter] postNotificationName:kChangeMarkAddChangeMarkNotification
                                                             object:string];
