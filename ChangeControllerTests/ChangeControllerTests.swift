582c0ff292443e93dee7b5dc22e406c9583f4f32
Christoffer Winterkvist <chris@zenangst.com>
2015-04-20 21:20:57 +0200

    Add test suit

---
+++ ChangeControllerTests/ChangeControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  ChangeControllerTests.swift
+//  ChangeControllerTests
+//
+//  Created by Christoffer Winterkvist on 20/04/15.
+//  Copyright (c) 2015 zenangst. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class ChangeControllerTests: XCTestCase {
+
+}
