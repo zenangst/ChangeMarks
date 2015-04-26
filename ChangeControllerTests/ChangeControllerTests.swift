fb9956348a9ee165ea52972bdcf3b8401b50f4dc
Christoffer Winterkvist <chris@zenangst.com>
2015-04-26 17:04:41 +0200

    Add testChangeIntersect

--- ChangeControllerTests/ChangeControllerTests.swift
+++ ChangeControllerTests/ChangeControllerTests.swift
@@ -22,4 +22,27 @@ class ChangeControllerTests: XCTestCase {
         XCTAssertTrue(controller.changes["testDocument"]?.count == 1)
     }
 
+    func testChangeIntersect() {
+        let controller = ChangeController()
+        let range1 = NSMakeRange(100,20)
+        let range2 = NSMakeRange(45,10)
+        let range3 = NSMakeRange(105,30)
+        let range4 = NSMakeRange(40,10)
+
+        controller.addChange(ChangeModel(range: range1, documentPath: "testDocument"))
+        controller.addChange(ChangeModel(range: range2, documentPath: "testDocument"))
+        controller.addChange(ChangeModel(range: range3, documentPath: "testDocument"))
+        controller.addChange(ChangeModel(range: range4, documentPath: "testDocument"))
+
+        XCTAssertTrue(controller.changes["testDocument"]?.count == 2)
+
+        let changes = controller.changes["testDocument"]
+        XCTAssertTrue(changes!.first!.location == 100)
+        XCTAssertTrue(changes!.first!.length == 35)
+
+        XCTAssertTrue(changes!.last!.location == 40)
+        XCTAssertTrue(changes!.last!.length == 15)
+
+    }
+
 }
