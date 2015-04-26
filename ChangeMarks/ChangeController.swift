2cde9b963deb3cfd3b5ad74962a46aa9eba24677
Christoffer Winterkvist <chris@zenangst.com>
2015-04-26 17:05:00 +0200

    Refactor

--- ChangeMarks/ChangeController.swift
+++ ChangeMarks/ChangeController.swift
@@ -10,47 +10,49 @@ import Foundation
 
 public class ChangeController {
 
-    public lazy var changes: Dictionary<String, [ChangeModel]> = {
-        return [:]
+  public lazy var changes: Dictionary<String, [ChangeModel]> = {
+    return [:]
     }()
 
-    func addChange(change: ChangeModel) {
-        if var changes = changes[change.documentPath] {
-            if let intersectChange = intersect(change) {
+  func addChange(change: ChangeModel) {
+    if var changes = changes[change.documentPath] {
+      if let intersectChange = intersect(change) {
 
-                if intersectChange.location < change.location {
-                    change.location = intersectChange.location
-                }
+        let newRangeLength = intersectChange.location! + intersectChange.length!
+        let oldRangeLength = change.location! + change.length!
 
-                let newRangeLength = intersectChange.location! + intersectChange.length!
-                let oldRangeLength = change.location! + intersectChange.length!
+        if change.location < intersectChange.location {
+          intersectChange.location = change.location
+        }
 
-                if (newRangeLength > oldRangeLength) {
-                    change.length = newRangeLength - change.location!
-                }
-
-            } else {
-                changes.append(change)
-               //changes[change.documentPath] = changes
-            }
+        if newRangeLength > oldRangeLength {
+          intersectChange.length = newRangeLength - intersectChange.location!
         } else {
-            changes[change.documentPath] = [change]
-        }
-    }
-
-    func intersect(change: ChangeModel) -> ChangeModel? {
-        var changes: [ChangeModel] = self.changes[change.documentPath]!
-        for oldChange in changes {
-            let a = NSMakeRange(change.location!, change.length!);
-            let b = NSMakeRange(oldChange.location!, oldChange.length!);
-            let intersection = NSIntersectionRange(a, b);
-
-            if intersection.location > 0 && intersection.length > 0 {
-                return oldChange
-            }
+          intersectChange.length = oldRangeLength - intersectChange.location!
         }
 
-        return nil
+      } else {
+        changes.append(change)
+        self.changes[change.documentPath] = changes
+      }
+    } else {
+      changes[change.documentPath] = [change]
     }
+  }
+
+  func intersect(change: ChangeModel) -> ChangeModel? {
+    var changes: [ChangeModel] = self.changes[change.documentPath]!
+    for oldChange in changes {
+      let a = NSMakeRange(change.location!, change.length!);
+      let b = NSMakeRange(oldChange.location!, oldChange.length!);
+      let intersection = NSIntersectionRange(a, b);
+
+      if intersection.location > 0 && intersection.length > 0 {
+        return oldChange
+      }
+    }
+
+    return nil
+  }
 
 }
